66f32ac10cf3dbb9e8c8f3759f4979153270fa7a
Nick O'Brien <nickobriencomposer@gmail.com>
2024-09-05 21:52:29 -0400

    seeds and models created

---
+++ Main/models/Deck.js
@@ -0,0 +1,36 @@
+//setting consts
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//setting Deck info types
+class Deck extends Model {}
+
+Deck.init(
+    //setting deck info
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        high_score: {
+            type: DataTypes.NUMBER,
+            allowNull: FALSE,
+        },
+    },
+    
+    //sequelize stuff
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'deck',
+    }
+);
+
+module.exports = Deck;
