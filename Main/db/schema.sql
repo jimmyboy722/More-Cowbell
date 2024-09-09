3e0a0adb20681729c7f916a0f86bcba52172ad00
jimmyboy722 <jeckman722@gmail.com>
2024-09-08 20:16:38 -0400

    mapped out skeleton of db transaction

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -20,3 +20,24 @@ CREATE TABLE users (
     hashed_password VARCHAR(100) NOT NULL,
     consecutive_correct_answers INTEGER DEFAULT 0
 );
+
+DO $$
+    DECLARE
+    -- IF WE NEED TO DECLARE ANY VARIABLES, THEY WILL BE LISTED HERE
+    
+    BEGIN -- START OF TRANSACTION
+
+    INSERT INTO cows (color, origin, coat, average_weight, purpose)
+    VALUES
+
+
+    INSERT INTO users (id, username, email, hashed_password, consecutive_correct_answers)
+    VALUES
+
+    RAISE NOTICE 'Transaction Complete!';
+
+    EXCEPTION -- IN CASE OF ERROR
+        WHEN OTHERS THEN
+            RAISE NOTICE 'An error has occurred!: %' SQLERRM; -- LOGS THE ERROR
+            ROLLBACK; --ROLLS BACK ALL CHANGES MADE IN CASE OF ERROR
+    END $$;-- SIGNIFIES END OF THE TRANSACTION
