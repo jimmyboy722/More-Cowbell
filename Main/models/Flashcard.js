f7e048894645d955d015f5b1230e7736b15f2ace
jimmyboy722 <jeckman722@gmail.com>
2024-09-12 19:57:47 -0400

    fixed auto-increment error

--- Main/models/Flashcard.js
+++ Main/models/Flashcard.js
@@ -6,48 +6,48 @@ const sequelize = require("../config/connection");
 class Flashcard extends Model {}
 
 Flashcard.init(
-//setting flashcards info
- {
-card: {
-  type: DataTypes.STRING,
-  allowNull: false,
-   primaryKey: true,
-  autoIncrement: true,
+  //setting flashcards info
+  {
+    card: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+      //autoIncrement: true,
+    },
+    cow: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true,
+    },
+    origin: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    coat_type: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    average_weight: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    purpose: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
- cow: {
-    type: DataTypes.STRING,
-    allowNull: false,
-     primaryKey: true,
-  },
-  origin: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  color: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  coat_type: {
-    type: DataTypes.STRING,
-   allowNull: false,
- },
- average_weight: {
-   type: DataTypes.STRING,
-   allowNull: false,
-  },
-  purpose: {
-   type: DataTypes.STRING,
-   allowNull: false,
-  },
- },
 
-//sequelize stuff
- {
-   sequelize,
-   freezeTableName: true,
-   underscored: true,
-   modelName: "Flashcard",
- }
+  //sequelize stuff
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Flashcard",
+  }
 );
 
 module.exports = Flashcard;
--- Main/models/User.js
+++ Main/models/User.js
@@ -17,7 +17,7 @@ User.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true,
+      //autoIncrement: true,
     },
     username: {
       type: DataTypes.STRING,
