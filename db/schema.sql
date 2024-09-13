34a2d944e079bc592cc0c7ea17849919576d73c9
Nick O'Brien <nickobriencomposer@gmail.com>
2024-09-13 00:12:01 -0400

    config

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,5 @@ CREATE DATABASE users_db;
 
 -- CREATING THE DATABASE FOR USERS --
 \c users_db;
-
-DROP DATABASE IF EXISTS flashcards_db;
-CREATE DATABASE flashcards_db;
-\c flashcards_db;
 -- CONNECTING TO THE DB --
 
