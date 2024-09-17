a1ce9ef2fd6732cb0e81240fbfb791566155914d
jimmyboy722 <jeckman722@gmail.com>
2024-09-16 22:20:33 -0400

    fixed db connection

--- config/connection.js
+++ config/connection.js
@@ -8,9 +8,10 @@ require("dotenv").config();
 const sequelize = new Sequelize({
   host: "localhost",
   port: "5432",
+  database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  dialect: "postgres"
+  dialect: "postgres",
 });
 
 // FOR TESTING THE DATABASE CONNECTION
